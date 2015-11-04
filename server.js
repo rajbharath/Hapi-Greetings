580b4fbed95907b938de5be0026a8dbcf7254c41
Raj Bharath K <rbrajbharath1@gmail.com>
2015-11-04 18:36:54 +0530

    Refactored the handlers

--- server.js
+++ server.js
@@ -37,28 +37,28 @@ server.route({
 
 server.route({
   path: '/cards/new',
-  method: 'GET',
-  handler: function(request, reply) {
-    reply.file('templates/new.html');
-  }
-});
-
-server.route({
-  path: '/cards/new',
-  method: 'POST',
-  handler: function(request, reply) {
-    //business logic
-    reply.redirect('/cards');
-  }
+  method: ['GET', 'POST'],
+  handler: newCardHandler
 });
 
 server.route({
   path: '/cards',
   method: 'GET',
-  handler: function(request, reply) {
-    reply.file('templates/cards.html');
-  }
+  handler: cardsHandler
 });
+
+function newCardHandler(request, reply) {
+  if (request.method === 'get') {
+    reply.file('templates/new.html');
+  } else {
+    //business logic
+    reply.redirect('/cards');
+  }
+}
+
+function cardsHandler(request, reply) {
+  reply.file('templates/cards.html');
+}
 
 server.start(function() {
   console.log('Listening on ' + server.info.uri);
